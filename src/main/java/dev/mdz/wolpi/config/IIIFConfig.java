48e8194f8d84b11b92f2702e003f245d651de369
Johannes Baiter <johannes.baiter@bsb-muenchen.de>
2025-11-10 09:18:18 +0100

    chore: formatting of IIIFConfig class

--- src/main/java/dev/mdz/wolpi/config/IIIFConfig.java
+++ src/main/java/dev/mdz/wolpi/config/IIIFConfig.java
@@ -13,92 +13,88 @@ import org.springframework.boot.context.properties.NestedConfigurationProperty;
 /// @param formats         The formats that can be requested, including allowed formats and
 ///                        preferred formats.
 public record IIIFConfig(
-    // TODO: Implement!
-    boolean restrictToSizes,
-    Limits limits,
-    Features features,
-    Qualities qualities,
-    Formats formats) {
+        // TODO: Implement!
+        boolean restrictToSizes, Limits limits, Features features, Qualities qualities, Formats formats) {
 
-  /// @param maxWidth  Maximum width that images can be returned in
-  /// @param maxHeight Maximum height that images can be returned in
-  /// @param maxArea   Maximum area `(width * height)` that images can be returned in.
-  public record Limits(int maxWidth, int maxHeight, long maxArea) {}
+    /// @param maxWidth  Maximum width that images can be returned in
+    /// @param maxHeight Maximum height that images can be returned in
+    /// @param maxArea   Maximum area `(width * height)` that images can be returned in.
+    public record Limits(int maxWidth, int maxHeight, long maxArea) {}
 
-  /// Optional IIIF Image API features that can be toggled.
-  ///
-  /// @param scaling             Flags to control allowed scaling features
-  /// @param region              Flags to control allowed region features
-  /// @param rotation            Flags to control allowed rotation features
-  /// @param profileLinkHeader   Whether to include the profile link header in responses
-  /// @param jsonLdMediaType     Whether to use JSON-LD media type for responses
-  /// @param cors                Whether to set the CORS header to `*`
-  /// @param canonicalLinkHeader Whether to include a canonical link header in responses
-  /// @param canonicalRedirect   Whether to redirect to the canonical URL for the image
-  /// @param baseUriRedirect     Whether to the info.json endpoint when accessing the base URI
-  ///                            without image parameters or `/info.json` suffix.
-  public record Features(
-      @NestedConfigurationProperty ScalingFeature scaling,
-      @NestedConfigurationProperty RegionFeature region,
-      @NestedConfigurationProperty RotationFeature rotation,
-      boolean profileLinkHeader,
-      boolean jsonLdMediaType,
-      boolean cors,
-      boolean canonicalLinkHeader,
-      boolean canonicalRedirect,
-      boolean baseUriRedirect) {}
+    /// Optional IIIF Image API features that can be toggled.
+    ///
+    /// @param scaling             Flags to control allowed scaling features
+    /// @param region              Flags to control allowed region features
+    /// @param rotation            Flags to control allowed rotation features
+    /// @param profileLinkHeader   Whether to include the profile link header in responses
+    /// @param jsonLdMediaType     Whether to use JSON-LD media type for responses
+    /// @param cors                Whether to set the CORS header to `*`
+    /// @param canonicalLinkHeader Whether to include a canonical link header in responses
+    /// @param canonicalRedirect   Whether to redirect to the canonical URL for the image
+    /// @param baseUriRedirect     Whether to the info.json endpoint when accessing the base URI
+    ///                            without image parameters or `/info.json` suffix.
+    public record Features(
+            @NestedConfigurationProperty ScalingFeature scaling,
+            @NestedConfigurationProperty RegionFeature region,
+            @NestedConfigurationProperty RotationFeature rotation,
+            boolean profileLinkHeader,
+            boolean jsonLdMediaType,
+            boolean cors,
+            boolean canonicalLinkHeader,
+            boolean canonicalRedirect,
+            boolean baseUriRedirect) {}
 
-  /// Optional scaling features that can be enabled or disabled
-  ///
-  /// @param byConfinedWidthHeight Whether to allow scaling by confined width and height with `!w,h`
-  /// @param byHeight              Whether to allow scaling by height with `,h`
-  /// @param byWidth               Whether to allow scaling by width with `w,`
-  /// @param byPercent             Whether to allow scaling by percent with `pct:`
-  /// @param byArbitraryDimensions Whether to allow scaling by arbitrary (non-aspect-ratio
-  ///                              preserving) dimensions with `w,h`
-  /// @param allowUpscaling        Allow upscaling of images, i.e. scaling to larger sizes than the
-  ///                              original
-  public record ScalingFeature(
-      boolean byConfinedWidthHeight,
-      boolean byHeight,
-      boolean byWidth,
-      boolean byPercent,
-      boolean byArbitraryDimensions,
-      boolean allowUpscaling) {}
+    /// Optional scaling features that can be enabled or disabled
+    ///
+    /// @param byConfinedWidthHeight Whether to allow scaling by confined width and height with `!w,h`
+    /// @param byHeight              Whether to allow scaling by height with `,h`
+    /// @param byWidth               Whether to allow scaling by width with `w,`
+    /// @param byPercent             Whether to allow scaling by percent with `pct:`
+    /// @param byArbitraryDimensions Whether to allow scaling by arbitrary (non-aspect-ratio
+    ///                              preserving) dimensions with `w,h`
+    /// @param allowUpscaling        Allow upscaling of images, i.e. scaling to larger sizes than the
+    ///                              original
+    public record ScalingFeature(
+            boolean byConfinedWidthHeight,
+            boolean byHeight,
+            boolean byWidth,
+            boolean byPercent,
+            boolean byArbitraryDimensions,
+            boolean allowUpscaling) {}
 
-  /// Optional region features that can be enabled or disabled
-  ///
-  /// @param byPercent Whether to allow regions specified by percent with `pct:x,y,w,h`
-  /// @param byPixels  Whether to allow regions specified by pixels with `x,y,w,h`
-  /// @param square    Whether to allow square regions with `square`
-  public record RegionFeature(boolean byPercent, boolean byPixels, boolean square) {}
+    /// Optional region features that can be enabled or disabled
+    ///
+    /// @param byPercent Whether to allow regions specified by percent with `pct:x,y,w,h`
+    /// @param byPixels  Whether to allow regions specified by pixels with `x,y,w,h`
+    /// @param square    Whether to allow square regions with `square`
+    public record RegionFeature(boolean byPercent, boolean byPixels, boolean square) {}
 
-  /// Optional rotation features that can be enabled or disabled
-  ///
-  /// @param mirroring          Whether to allow mirroring of images on the vertical axis with `!`
-  /// @param by90DegreeRotation Whether to allow 90 degree rotations with `0,90,180,270`
-  /// @param arbitrary          Whether to allow arbitrary rotations with `arbitrary:angle`
-  public record RotationFeature(boolean mirroring, boolean by90DegreeRotation, boolean arbitrary) {}
+    /// Optional rotation features that can be enabled or disabled
+    ///
+    /// @param mirroring          Whether to allow mirroring of images on the vertical axis with `!`
+    /// @param by90DegreeRotation Whether to allow 90 degree rotations with `0,90,180,270`
+    /// @param arbitrary          Whether to allow arbitrary rotations with `arbitrary:angle`
+    public record RotationFeature(boolean mirroring, boolean by90DegreeRotation, boolean arbitrary) {}
 
-  /// Allowed and default "qualities" for images.
-  ///
-  /// See [IIIF Image API 3.0 Quality](https://iiif.io/api/image/3.0/#quality) for more details.
-  ///
-  /// **Note:** This is explicitly not restricted to certain values with an enum,
-  /// since we want to allow custom qualities to be defined by extensions.
-  ///
-  /// @param defaultQuality The quality to use if `default` is requested.
-  /// @param allowed        List of allowed qualities that can be requested.
-  public record Qualities(String defaultQuality, List<String> allowed) {}
+    /// Allowed and default "qualities" for images.
+    ///
+    /// See [IIIF Image API 3.0 Quality](https://iiif.io/api/image/3.0/#quality) for more details.
+    ///
+    /// **Note:** This is explicitly not restricted to certain values with an enum,
+    /// since we want to allow custom qualities to be defined by extensions.
+    ///
+    /// @param defaultQuality The quality to use if `default` is requested.
+    /// @param allowed        List of allowed qualities that can be requested.
+    public record Qualities(String defaultQuality, List<String> allowed) {}
 
-  /// Allowed and preferred formats for images.
-  ///
-  /// **Note:** This is explicitly not restricted to certain values with an enum,
-  /// since we want to allow custom formats to be defined by extensions.
-  ///
-  /// @param allowed   List of allowed formats that can be requested, as file extensions without the
-  ///                  leading dot.
-  /// @param preferred List of preferred formats that should be requested by clients, as file
-  ///                  extensions without the leading dot.
-  public record Formats(List<String> allowed, List<String> preferred) {}
+    /// Allowed and preferred formats for images.
+    ///
+    /// **Note:** This is explicitly not restricted to certain values with an enum,
+    /// since we want to allow custom formats to be defined by extensions.
+    ///
+    /// @param allowed   List of allowed formats that can be requested, as file extensions without the
+    ///                  leading dot.
+    /// @param preferred List of preferred formats that should be requested by clients, as file
+    ///                  extensions without the leading dot.
+    public record Formats(List<String> allowed, List<String> preferred) {}
 }
